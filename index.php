491e3fbdb3f2e9bba1deb4d6480c5648f4d4fa78
T-Welsh <you@example.comsalthook@gmail.com>
2022-07-07 10:52:11 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--============ HEADER TO BE SPLIT OFF INTO .INCL FILE ============-->
+    <header>
+        HEADER
+    </header>
+
+    <main>
+        <!--============ HERO ============-->
+        <section>
+            HERO SECTION
+        </section>
+
+        <!--============ INTRO ============-->
+        <section>
+            INTRO SECTION
+        </section>
+
+        <!--============ ABOUT ============-->
+        <section>
+            ABOUT SECTION
+        </section>
+
+        <!--============ SERVICES ============-->
+        <section>
+            SERVICES SECTION
+        </section>
+
+        <!--============ QUOTE ============-->
+        <section>
+            QUOTE SECTION
+        </section>
+
+        <!--============ CONTACT ============-->
+        <section>
+            CONTACT SECTION
+        </section>
+    </main>
+
+    <!--============ FOOTER TO BE SPLIT OFF INTO .INCL FILE ============-->
+    <footer>
+        FOOTER
+    </footer>
+
+</body>
+</html>
